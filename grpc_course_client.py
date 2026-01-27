ff8af29ed8bf9289162fae7cdd8aca139137138b
Anton <antonpleshakov.job@gmail.com>
2026-01-27 16:19:29 +0300

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом.

--- grpc_course_client.py
+++ grpc_course_client.py
@@ -8,6 +8,8 @@ channel = grpc.insecure_channel('localhost:50051')
 stub = course_service_pb2_grpc.CourseServiceStub(channel)
 response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
 # Отправляем запрос
-print(f'course_id: "{response.course_id}"')
+
+print(response)
+""" print(f'course_id: "{response.course_id}"')
 print(f'title: "{response.title}"')
-print(f'description: "{response.description}"')
+print(f'description: "{response.description}"') """
